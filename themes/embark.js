939a0cf897506d9d10a83ab8485e53aa82b778ea
Amy Dang <kirontoo@gmail.com>
2021-05-09 19:35:16 -0700

    add theme

---
+++ themes/embark.js
@@ -0,0 +1,60 @@
+module.exports = {
+	name: 'embark',
+	displayName: 'Embark',
+	theme: {
+		background: {
+			default: '#1e1c31',
+			success: '#a1efd3',
+			notice: '#ffe6b3',
+			warning: '#f2b482',
+			danger: '#f48fb1',
+			surprise: '#d4bfff',
+			info: '#91ddff'
+		},
+		foreground: {
+			default: '#cbe3e7',
+			success: '#1e1c31',
+			notice: '#1e1c31',
+			warning: '#1e1c31',
+			danger: '#1e1c31',
+			surprise: '#1e1c31',
+			info: '#1e1c31'
+		},
+		highlight: {
+			default: '#d4bfff',
+			xxs: '#3e3859',
+			xs: '#3e3859',
+			sm: '#3e3859',
+			md: '#3e3859',
+			lg: '#3e3859',
+			xl: '#d4bfff'
+		},
+		styles: {
+			sidebar: {
+				background: {
+					default: '#1e1c31'
+				}
+			},
+			dialog: {
+				background: {
+					default: '#100e23'
+				}
+			},
+			paneHeader: {
+				background: {
+					success: '#a1efd3',
+					notice: '#ffe6b3',
+					warning: '#f2b482',
+					danger: '#f48fb1',
+					surprise: '#d4bfff',
+					info: '#87dfeb'
+				}
+			},
+			transparentOverlay: {
+				background: {
+					default: 'rgba(40, 42, 54, 0.5)'
+				}
+			}
+		}
+	}
+}
